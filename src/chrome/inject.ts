bb707a60847c3c6d2a7abc056f821d03f790b6fb
apoorvlathey <latheyapoorv@gmail.com>
2023-01-12 05:37:08 +0530

    feat: connect to injected wallet via modal

--- src/chrome/inject.ts
+++ src/chrome/inject.ts
@@ -1,51 +1,22 @@
-const createButton = () => {
-  const btnGroup = document.querySelector('[role="group"]');
-  const div = document.createElement("div");
-  div.style.setProperty("padding-bottom", "1rem");
-  const btn = document.createElement("button");
-  btn.id = "lens-share";
-  btn.style.setProperty("background-color", "green");
-  btn.style.setProperty("color", "white");
-  btn.style.setProperty("border-radius", "0.7rem");
-  btn.style.setProperty("font-weight", "bold");
-  btn.addEventListener(
-    "mouseover",
-    () => {
-      btn.style.setProperty("cursor", "pointer");
-    },
-    false
-  );
-  btn.addEventListener(
-    "mouseout",
-    () => {
-      btn.style.setProperty("cursor", "default");
-    },
-    false
-  );
-  btn.innerHTML = "Share on Lens 🌿";
-  btn.onclick = () => {
-    window.open(`https://lens-share.apoorv.xyz/?url=${window.location.href}`);
-  };
-  div.appendChild(btn);
-  btnGroup?.appendChild(div);
-};
-
 const init = async () => {
-  const checkIfTwitterLoaded = () => {
-    if (document.querySelector('[role="group"]')) {
-      clearInterval(btnGroupCheckTimer);
-
-      // create button in our target element
-      createButton();
-    }
-  };
-
-  // wait for our target element to load
-  var btnGroupCheckTimer = setInterval(checkIfTwitterLoaded, 200);
+  // inject impersonator.js into webpage
+  try {
+    let script = document.createElement("script");
+    script.setAttribute("type", "text/javascript");
+    script.src = chrome.runtime.getURL("/static/js/impersonator.js");
+    script.onload = async function () {
+      // @ts-ignore
+      this.remove();
+    };
+    document.head
+      ? document.head.prepend(script)
+      : document.documentElement.prepend(script);
+  } catch (e) {
+    console.log(e);
+  }
 };
 
-// wait for `load` event, then call `init`
-window.addEventListener("load", init, false);
+init();
 
 // to remove isolated modules error
 export {};
